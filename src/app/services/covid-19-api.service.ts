ae54552a5589f33187c42c0dedc87511641ba5a8
Matthew Bischoff <matthew.john.bischoff@gmail.com>
2020-03-31 17:08:00 -0400

    cache api calls

--- src/app/services/covid-19-api.service.ts
+++ src/app/services/covid-19-api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { map, repeat } from 'rxjs/operators';
 import { TotalCount } from '../models/total-count.model';
 import { CountryModel } from '../models/country.model';
 import { SummaryModel, SummaryResponse } from '../models/summary.model';
@@ -11,36 +11,44 @@ import { SummaryModel, SummaryResponse } from '../models/summary.model';
 })
 export class Covid19ApiService {
 
+  cache = {};
+
   constructor(private http: HttpClient) { }
 
   totalsSinceDayOne(countrySlug: string, status: 'confirmed' | 'recovered' | 'deaths'): Observable<TotalCount[]> {
-    return this.http.get(`https://api.covid19api.com/country/${countrySlug}/status/${status}`).pipe(map(resp => {                              
-      return resp as TotalCount[];
-    }));
+    const url = `https://api.covid19api.com/country/${countrySlug}/status/${status}`;
+    return this.getOrLoadFromCache(url);
   }
 
   countryTotalsSinceDayOne(countrySlug: string, status: 'confirmed' | 'recovered' | 'deaths'): Observable<TotalCount[]> {
-    return this.http.get(`https://api.covid19api.com/total/dayone/country/${countrySlug}/status/${status}`).pipe(map(resp => {                  
-      return resp as TotalCount[];
-    }));
+    const url = `https://api.covid19api.com/total/dayone/country/${countrySlug}/status/${status}`;
+    return this.getOrLoadFromCache(url);
   }
 
 
-  totalsByCountry(countrySlug: string, status: 'confirmed' | 'recovered' | 'deaths'): Observable<TotalCount[]>{
-    return this.http.get(`https://api.covid19api.com/country/${countrySlug}/status/${status}`).pipe(map(resp => {
-      return resp as TotalCount[];
-    }));
+  totalsByCountry(countrySlug: string, status: 'confirmed' | 'recovered' | 'deaths'): Observable<TotalCount[]> {
+    const url = `https://api.covid19api.com/country/${countrySlug}/status/${status}`;
+    return this.getOrLoadFromCache(url);
   }
 
   getCountries(): Observable<CountryModel[]> {
-    return this.http.get(`https://api.covid19api.com/countries`).pipe(map((resp) => {
-      return resp as CountryModel[];
-    }));
+    const url = `https://api.covid19api.com/countries`;
+    return this.getOrLoadFromCache(url);
   }
 
   getSummary(): Observable<SummaryModel[]> {
-    return this.http.get(`https://api.covid19api.com/summary`).pipe(map((resp: SummaryResponse) => {
-      return resp.Countries as SummaryModel[];
-    }));
+    const url = `https://api.covid19api.com/summary`;
+    return this.getOrLoadFromCache(url).pipe(map(resp => resp.Countries));
+  }
+
+  private getOrLoadFromCache(url): Observable<any> {
+    if (this.cache[url]) {
+      return of(this.cache[url] as TotalCount[]);
+    } else {
+      return this.http.get(url).pipe(map(resp => {
+        this.cache[url] = resp;
+        return resp;
+      }));
+    }
   }
 }
